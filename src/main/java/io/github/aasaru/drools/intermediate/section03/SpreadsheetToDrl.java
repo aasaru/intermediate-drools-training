061ab42e382e9e9237bb31b255485541e0243c02
Juhan Aasaru <Juhan.Aasaru@nortal.com>
2022-09-06 10:26:01 +0300

    remove full path

--- src/main/java/io/github/aasaru/drools/intermediate/section03/SpreadsheetToDrl.java
+++ src/main/java/io/github/aasaru/drools/intermediate/section03/SpreadsheetToDrl.java
@@ -6,20 +6,22 @@ import org.drools.core.builder.conf.impl.DecisionTableConfigurationImpl;
 import org.drools.core.io.impl.FileSystemResource;
 import org.kie.internal.builder.DecisionTableConfiguration;
 
+import java.io.File;
+import java.net.URL;
+import java.util.Objects;
+
 public class SpreadsheetToDrl {
 
     public static void main(String[] args) {
 
         int step = Common.promptForStep(3, new String[]{"RULES"}, 1, 12);
 
-
-        // TODO - remove absolute path and make work when running from JAR
-
         DecisionTableConfiguration decisionTableConfiguration = new DecisionTableConfigurationImpl();
 
         FileSystemResource
                 spreadsheetFile =
-                new FileSystemResource("/Users/juhan/spaces/github/intermediate-drools-training/src/main/resources/io/github/aasaru/drools/intermediate/section03/step" + step + "/step" + step + ".xls");
+                new FileSystemResource(getFile("io/github/aasaru/drools/intermediate/section03/step" + step + "/step" + step + ".xls"));
+
         String generatedDrl = DecisionTableFactory.loadFromResource(spreadsheetFile, decisionTableConfiguration);
 
         System.out.println();
@@ -29,8 +31,12 @@ public class SpreadsheetToDrl {
         System.out.println();
         System.out.println(" ######################## END ######################## ");
         System.out.println();
+    }
 
-
+    private static File getFile(String fileName) {
+        ClassLoader classLoader = SpreadsheetToDrl.class.getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+        return new File(Objects.requireNonNull(resource).getFile());
     }
 
 }
